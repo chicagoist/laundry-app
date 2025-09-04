2721800c70528f903a72a34915c3dd19f7bc2232
chicagoist <le.legioner@gmail.com>
2025-09-04 14:23:06 +0200

    Мой вариант решения до оптимизации. Работает.

--- src/main/kotlin/com/github/chicagoist/laundryapp/app/Main.kt
+++ src/main/kotlin/com/github/chicagoist/laundryapp/app/Main.kt
@@ -7,13 +7,23 @@ fun main() {
 
     print("Машина (Washer или Dryer): ")
     val machine = readln()
+    if (machine != "Washer" && machine != "Dryer") {
+        println("Неверное название машины")
+        return
+    }
 
     println("Выберите состояние:")
     println("1 - working")
     println("2 - broken")
     println("3 - maintenance")
 
-    val status = when (readln().toIntOrNull()) {
+    val choice = readln().toIntOrNull()
+    if (choice == null || choice !in 1..3) {
+        println("Ошибка: введите цифру 1, 2 или 3!")
+        return
+    }
+
+    val status = when (choice) {
         1 -> "working"
         2 -> "broken"
         3 -> "maintenance"
@@ -23,8 +33,20 @@ fun main() {
         }
     }
 
+    if (choice == 2 || choice == 3) {
+        println("Ошибка: машина не может быть в этом состоянии!")
+    }
+
+
+
     print("Время работы в минутах: ")
     val worktime = readln().toIntOrNull() ?: 0
+    if (worktime <= 0) {
+        println("Время должно быть > 0!")
+        return
+    } else if (worktime > 180) {
+        println("Время превышает 3 часа")
+    }
 
     println(
         """
